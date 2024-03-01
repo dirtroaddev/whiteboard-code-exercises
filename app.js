a5eb14404a43bff43c2688bf5ed2f0274d4f0b3c
dirtroaddev <rongoodwindev@gmail.com>
2024-02-29 22:42:35 -0500

    Finished another seciton

--- app.js
+++ app.js
@@ -348,3 +348,18 @@
 // console.log(peakFinder([4, 6, 9, 4, 2, -7, 2, -4, 5])); // => [2, 6, 8]
 
 
+function divisibleByThreePairSum(arr) {
+    
+}
+
+
+
+
+
+
+
+const arr1 = divisibleByThreePairSum([1, 6, 3, 4, 2, 0]);
+console.log(arr1) // => [[0, 4], [1, 2], [1, 5], [2, 5], [3, 4]]
+
+const arr2 = divisibleByThreePairSum([8, 3, 5, 9, 2]);
+console.log(arr2) // => [[1, 3]]
